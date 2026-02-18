f54a3a02df66f0dfb067e7cc2a2dd573c791e53e
Lukas <l.jastramskas@gmail.com>
2026-02-18 13:34:26 +0200

    Railway fix

--- server.js
+++ server.js
@@ -7,44 +7,24 @@ const app = express()
 app.use(cors())
 app.use(express.static("public"))
 
-
 const server = http.createServer(app)
+
 const io = new Server(server, {
   cors: {
     origin: "*"
   }
 })
 
-let users = []
-
 io.on("connection", (socket) => {
-  console.log("New user connected:", socket.id)
-
-  socket.on("join", (username) => {
-    users.push({ id: socket.id, username })
-    io.emit("chat message", {
-      username: "System",
-      message: `${username} prisijungė prie ChatX`
-    })
-  })
+  console.log("User connected")
 
   socket.on("chat message", (data) => {
     io.emit("chat message", data)
   })
-
-  socket.on("disconnect", () => {
-    const user = users.find(u => u.id === socket.id)
-    if (user) {
-      io.emit("chat message", {
-        username: "System",
-        message: `${user.username} atsijungė`
-      })
-      users = users.filter(u => u.id !== socket.id)
-    }
-  })
 })
 
 const PORT = process.env.PORT || 3000
-server.listen(PORT, () => {
-  console.log(`ChatX server running on port ${PORT}`)
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on ${PORT}`)
 })
